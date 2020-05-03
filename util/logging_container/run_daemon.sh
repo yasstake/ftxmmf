8656a828cfc059d4913be9d706168f87f6317ed8
yass <yass@takibi.net>
2020-05-04 00:23:08 +0900

    change log dir

--- util/logging_container/run_daemon.sh
+++ util/logging_container/run_daemon.sh
@@ -4,12 +4,12 @@ docker kill $(docker ps -q -a)
 docker rm $(docker ps -q -a)
 
 echo 'start daemon A'
-docker run --name WORKER-A -d -v /tmp:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
+docker run --name WORKER-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
 
 sleep 10
 
 echo 'start daemon B'
-docker run --name WORKER-B -d -v /tmp:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
+docker run --name WORKER-B -d -v /biglog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
 
 
 
