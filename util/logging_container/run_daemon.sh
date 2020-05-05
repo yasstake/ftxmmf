bb18b24d1de971bc37151318f5a97867f002b8f3
takeo <yass@takibi.net>
2020-05-05 20:57:04 +0900

    change process id to datetime to random (for docker)

--- util/logging_container/run_daemon.sh
+++ util/logging_container/run_daemon.sh
@@ -4,13 +4,13 @@ docker kill $(docker ps -q -a)
 docker rm $(docker ps -q -a)
 
 echo 'start daemon A'
-docker run --name BF-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
+docker run --name BF-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
 docker run --name FTX-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
 
 sleep 30
 
 echo 'start daemon B'
-docker run --name BF-B -d -v /biglog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/net2log.sh
+docker run --name BF-B -d -v /biglog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
 docker run --name FTX-B -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
 
 
