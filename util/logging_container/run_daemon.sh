9b7e8cfe2bc3caabfe7930944d40b8c63c726030
takeo <yass@takibi.net>
2020-05-09 12:46:34 +0900

    add bybit client

--- util/logging_container/run_daemon.sh
+++ util/logging_container/run_daemon.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-docker kill $(docker ps -q -a)
-docker rm $(docker ps -q -a)
+#docker kill $(docker ps -q -a)
+#docker rm $(docker ps -q -a)
+
+NAME=$1
 
 echo 'start daemon A'
-docker run --name BF-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
+docker run --name BF-A${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
 sleep 1
-docker run --name FTX-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
+docker run --name FTX-A${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
 sleep 1
-docker run --name BB-A -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bb2log.sh
+docker run --name BB-A${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bb2log.sh
 
-
+echo 'first daemon start----'
 sleep 30
 
 echo 'start daemon B'
-docker run --name BF-B -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
+docker run --name BF-B${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bf2log.sh
 sleep 1
-docker run --name FTX-B -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
+docker run --name FTX-B${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/ftx2log.sh
 sleep 1
-docker run --name BB-B -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bb2log.sh
+docker run --name BB-B${NAME} -d -v /bitlog:/bitlog --restart=always -t ftxmmf bash /ftxmmf/bb2log.sh
 
 
 
